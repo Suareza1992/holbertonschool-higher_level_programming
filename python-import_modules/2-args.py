ac5dcf5af350cfc229477f85c9efde4f466c5b6b
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-05-10 12:57:03 -0400

    Task 2 typo correction

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len_args = len(sys.args) - 1
+    len_args = len(sys.argv) - 1
     if len_args == 1:
         print("{} argument:".format(len_args))
         print("1: {}".format(sys.argv[1]))
