6375363ffcf7a8fb365f18e97b879cd558366e9b
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-05-28 22:38:38 -0400

    Task 1 PEP8 third try

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """Function that writes a string to a text file and returns the number of characters"""
+    """Writes string to text file and returns number of characters"""
 
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
