a9cd233f35d15760a1b7383a9c9fb861c1d5a98c
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-02 17:49:37 -0400

    Typo corrections made to the code itself

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,8 +2,8 @@ import requests
 import csv
 
 
-def fetch_and_print_post():
-    response = request.get('https://jsonplaceholder.typicode.com/posts')
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     print(f"Status Code: {response.status_code}")
 
@@ -19,21 +19,26 @@ def fetch_and_print_post():
 def fetch_and_save_posts():
     url = requests.get('https://jsonplaceholder.typicode.com/posts')
 
+    if url.status_code == 200:
         posts = url.json()
 
         structured_post = [
-            {"id": post["id"], "title": post["title"], "body: post["body"]}
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
             for post in posts
         ]
 
         csv_columns = ["id", "title", "body"]
 
-        with open("posts.csv", "w" newline='', encoding='utf-8) as csvfile:
+        with open("posts.csv", "w", newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writerow(post)
+            writer.writeheader()
+
+            for post in structured_post:
+                writer.writerow(post)
 
         print("Data has been written to posts.csv successfully.")
     else:
         print(f"Failed to fetch posts. Status code: {url.status_code}")
 
-    fetch_and_print_posts()
+
+fetch_and_print_posts()
