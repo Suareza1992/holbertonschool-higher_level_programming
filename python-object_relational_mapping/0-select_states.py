9014038dd782b679302906544d8cbdce1c4f88d2
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-24 16:54:21 -0400

    Changing python address

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
-!/usr/bin/python3
-"""Lists all the states from a specified database."""
-import MySQLdb
+#!/usr/bin/python3
+# Lists all states from a specified database.
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
     user, passwd, db_name = sys.argv[1:4]
     db = MySQLdb.connect('localhost', user, passwd, db_name, 3306)
     cur = db.cursor()
-    # Now we're ready for a query.
+    # Ready for a query.
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print("({}, '{}')".format(*row))
-    #Clean Up
+    # Clean Up
     cur.close
     db.close()
