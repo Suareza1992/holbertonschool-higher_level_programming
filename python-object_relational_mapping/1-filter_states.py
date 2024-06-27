b280463dcd223cb7e4580ee36b2b6940ac6785b9
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-26 20:45:33 -0400

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all the states that starts with the letter 'N' from a specified databse."""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user, passwd, db_name = sys.argv[1:4]
+    db = MySQLdb.connect('localhost', user, passwd, db_name)
+    cur = db.cursor()
+    # Query ready.
+    cur.execute("SELECT * FROM states" "WHERE name LIKE BINARY 'N%' " "ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Clean up.
+    cur.close()
+    db.close()
