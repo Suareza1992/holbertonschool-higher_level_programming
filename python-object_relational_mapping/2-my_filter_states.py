b5e98481c35e7dd253224fdfcc6523825880547b
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-27 00:12:38 -0400

    Task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states table of a specified table where name matches the argument
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    user, passwd, db_name, toMatch = sys.argv[1:5]
+    db = MySQLdb.connect(user= user, password=passwd, database=db_name)
+    cur = db.cursor()
+    # Query ready
+    cur.execute("SELECT * FROM states "
+                "WHERE BINARY name='{}' "
+                "ORDER BY id".format(toMatch))
+    for row in cur.fetchall():
+        print(row)
+    # Clean up
+    cur.close()
+    db.close()
