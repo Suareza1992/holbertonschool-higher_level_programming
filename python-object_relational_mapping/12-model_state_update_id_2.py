b3d59482e28f1b09d51c829d15aefb49e9f8540b
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-27 03:28:36 -0400

    Task 12 CGPT correction

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
 """
-Adds the State object “Louisiana” to the database passed as an argument
+    Updates the name of a State object from the database hbtn_0e_6_usa
+    where id = 2 to 'New Mexico'
 """
-from sys import argv
-import sqlalchemy
-from sqlalchemy import create_engine, orm
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(*argv[1:4]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = orm.sessionmaker(bind=engine)()
+    # Retrieve command-line arguments
+    user, passwd, db_name = sys.argv[1:4]
 
-    toModify = session.query(State).get(2)
-    toModify.name = 'New Mexico'
-    session.commit()
+    # Create an engine and bind it to the metadata of the Base class
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db_name}')
+    Base.metadata.bind = engine
 
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Retrieve the state with id = 2
+    to_modify = session.get(State, 2)
+    
+    # Update the state's name if it exists
+    if to_modify:
+        to_modify.name = 'New Mexico'
+        session.commit()
+
+    # Clean up
     session.close()
+
