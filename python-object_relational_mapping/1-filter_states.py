acb31c8d7b2e720c27bc2ccd818f7b2aa5f5379b
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-26 23:17:48 -0400

    Task 1 typos

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Lists all the states that starts with the letter 'N' from a specified databse."""
+"""
+Lists all the states that starts with the letter 'N' from a specified databse.
+"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
     user, passwd, db_name = sys.argv[1:4]
-    db = MySQLdb.connect('localhost', user, passwd, db_name)
+    db = MySQLdb.connect(user=user, password=passwd, database=db_name)
     cur = db.cursor()
     # Query ready.
-    cur.execute("SELECT * FROM states" "WHERE name LIKE BINARY 'N%' " "ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        print("({}, '{}')".format(*row))
     # Clean up.
-    cur.close()
+    cur.close
     db.close()
